f294bc1065ca3ba3af59b5bf531c9c5f5def4938
Kyle Sayers <kylesayrs@gmail.com>
2025-01-04 05:10:39 +0000

    comment about phi3 processor overflows
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- examples/multimodal_vision/phi3_example.py
+++ examples/multimodal_vision/phi3_example.py
@@ -54,6 +54,8 @@ def tokenize(sample):
     )
 
 
+# long data lengths produced by the phi3_vision processor
+# can lead to integer overflows when mapping, avoid with writer_batch_size
 ds = ds.map(tokenize, writer_batch_size=1, remove_columns=ds.column_names)
 
 
